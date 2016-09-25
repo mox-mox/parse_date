18b09fafe522bb5ec555e639b3d8711f094be707
mox <de_mox@zoho.com>
2016-09-25 15:40:32 +0200

    Behavioural tweaks. Seems to work. TODO: Make available as lib.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: parse_date
 
 
-parse_date:
+parse_date: main.cpp parse_date.cpp
 	g++ main.cpp parse_date.cpp -Wall -Wextra -Wpedantic -Werror -Wfatal-errors -std=c++17 -o parse_date
 
 run: parse_date
--- parse_date.cpp
+++ parse_date.cpp
@@ -51,108 +51,85 @@ std::time_t parse_time(const std::string& entry)
 	//std::string time="0:0:0";
 
 
-	if( !std::getline(entry_stream, date, '/').eof())
+	if( !std::getline(entry_stream, date, '/').eof() || date.find("-") != std::string::npos)
 	{
-		//std::cout<<"Given date is: "<<date<<std::endl;
-		if( date.empty())
+		if(date.find(":") != std::string::npos)
 		{
-			std::cout<<"empty date entered"<<std::endl;
-		}
-		if( date.find(":") == std::string::npos )
-		{
-			uint16_t dates[3];
-			int i = -1;
-			std::stringstream date_stream(date);
-			std::string temp;
-			while( !date_stream.eof() && std::getline(date_stream, temp, '-') && ++i <= 3 )
-			{
-				//std::cout<<"i: "<<i<<": ";
-				dates[i] = std::stoi(temp, 0, 10);
-				std::cout<<dates[i]<<std::endl;
-			}
-			i++;
-
-			std::cout<<"day: if("<<i<<"--): ";
-			if(i-- > 0)
-			{
-				std::cout<<i<<std::endl;
-				time_tm.tm_mday = dates[i];
-			}
-
-			std::cout<<"month: if("<<i<<"--): ";
-			if(i-- > 0)
-			{
-				std::cout<<i<<std::endl;
-				time_tm.tm_mon = dates[i]-1;
-			}
-
-			std::cout<<"year: if("<<i<<"--): ";
-			if(i-- > 0)
-			{
-				std::cout<<i<<std::endl;
-				time_tm.tm_year = dates[i]-1900;
-			}
-		}
-		else
-		{
-			if( date.find("-") == std::string::npos )
-			{
-				time = date;
-			}
-			else
-			{
-				throw std::invalid_argument("Passed date mixed up date separator (-) and time separator(:) in the date part.");
-			}
+			throw std::invalid_argument("Passed date mixed up date separator (-) and time separator(:) in the date part.");
 		}
 
+		uint16_t dates[3];
+		int i = -1;
+		std::stringstream date_stream(date);
+		std::string temp;
+		while( !date_stream.eof() && std::getline(date_stream, temp, '-') && ++i <= 3 )
+		{
+			dates[i] = std::stoi(temp, 0, 10);
+		}
+		i++;
 
+		if(i-- > 0)
+		{
+			time_tm.tm_mday = dates[i];
+		}
 
-		std::getline(entry_stream, time);
+		if(i-- > 0)
+		{
+			time_tm.tm_mon = dates[i]-1;
+		}
+
+		if(i-- > 0)
+		{
+			time_tm.tm_year = dates[i]-1900;
+		}
 	}
 	else
 	{
 		time = date;
 	}
 
+
+
+	std::getline(entry_stream, time);
+
 	// parse time
+	if( time.find("-") != std::string::npos )
 	{
-		std::cout<<"Given time is: "<<time<<std::endl;
-		if( time.empty())
-		{
-			std::cout<<"empty time entered"<<std::endl;
-			//time="0:0:0";
-		}
-		if( time.find("-") != std::string::npos )
-		{
-			throw std::invalid_argument("Passed time mixed up date separator (-) and time separator(:) in the time part.");
-		}
+		throw std::invalid_argument("Passed time mixed up date separator (-) and time separator(:) in the time part.");
+	}
 
-		uint16_t times[3];
-		int i = -1;
-		std::stringstream time_stream(time);
-		std::string temp;
-		while( !time_stream.eof() && std::getline(time_stream, temp, ':') && ++i <= 3 )
-		{
-			std::cout<<"i: "<<i<<": ";
-			times[i] = std::stoi(temp, 0, 10);
-			std::cout<<times[i]<<std::endl;
-		}
-		i++;
+	uint16_t times[3];
+	int i = -1;
+	std::stringstream time_stream(time);
+	std::string temp;
+	while( !time_stream.eof() && std::getline(time_stream, temp, ':') && ++i <= 3 )
+	{
+		times[i] = std::stoi(temp, 0, 10);
+	}
+	i++;
 
-		if( 0 < i )
-		{
-			time_tm.tm_hour = times[0];
-		}
+	if( 0 < i )
+	{
+		time_tm.tm_hour = times[0];
+	}
 
-		if( 1 < i )
-		{
-			time_tm.tm_min = times[1];
-		}
+	if( 1 < i )
+	{
+		time_tm.tm_min = times[1];
+	}
+	else if( 0 < i)
+	{
+		time_tm.tm_min = 0;
+	}
 
-		if( 2 < i )
-		{
-			time_tm.tm_sec = times[2];
-		}
+
+	if( 2 < i )
+	{
+		time_tm.tm_sec = times[2];
+	}
+	else if( 0 < i)
+	{
+		time_tm.tm_sec = 0;
 	}
 
 	return std::mktime(&time_tm);
